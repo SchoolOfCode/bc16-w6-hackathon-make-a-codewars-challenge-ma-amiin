5ec51edee33de8b6741407ee6cfae55d5381cdef
Guy Dunton <guydunton@economist.com>
2024-03-15 16:24:03 +0000

    Completed solution

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 /**
- * Hello challenger! You're a teacher at a school and you've notice that not all the names on the school register have been capitalised. 
+ * Hello challenger! You're a teacher at a school and you've notice that not all the names on the school register have been capitalised.
  *  Your task is to write a function named `capitaliseNames` which takes in an array and returns back the array but with all the non-capitalised words having been capitalised
  *
  *
  * A few examples:
- *    `transformLength([ben, Henry, Harry, mike, jessica, ahmed])` should return [Ben, Henry, Harry, Mike, Jessica, Ahmed] 
+ *    `transformLength([ben, Henry, Harry, mike, jessica, ahmed])` should return [Ben, Henry, Harry, Mike, Jessica, Ahmed]
  *    `transformLength([Tom, Felix, Sandra, Juan])` should return [Tom, Felix, Sandra, Juan] as they were all already capitalised
  */
 export function capitalizeNames(array) {
-  //write you code here!!
-  }
+  const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+  const upperCase = lowerCase.toUpperCase();
 
-  
+  //write you code here!!
+  return array.map((name) => {
+    const index = lowerCase.indexOf(name[0]);
+    return name.replace(/^[a-z]/, upperCase[index]);
+  });
+}
